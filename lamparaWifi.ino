f517b0d677169d275203c67800e516777942fa32
Trastejant <nano@trastejant.com>
2017-01-09 23:55:54 +0100

    Añadido control de intensidad de luz

--- lamparaWifi.ino
+++ lamparaWifi.ino
@@ -8,11 +8,14 @@
   #include <avr/power.h>
 #endif
 
-//Datos de conexion a la red
-const char* ssid = "NombreDeTuRed";
-const char* password = "Contraseña";
+const char* ssid = "MOVISTARTL";
+const char* password = "Martin89";
 
-#define PIN 6
+//Configuracion de casa de Sarah
+//const char* ssid = "PAPERCHASE";
+//const char* password = "A8DAFDBD7CE941CF30D8";
+
+#define PIN 4
 #define NUMPIXELS 16
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
@@ -20,38 +23,42 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ80
 String R,G,B;
 int Ri, Gi, Bi;
 
+String intensidad;
+int Ii;
+
 ESP8266WebServer server(80);
 WiFiClient client;
 
 const int led = 13;
 
-String web = "<!DOCTYPE HTML><html><head><title>Lampara</title></head><body><h1> <FONT SIZE=\"5\" COLOR=\"red\"> Colores </h1>Selecciona un color: <form action=\"\"><input name=\"color\" type=\"color\" style='width:300px; height:300px'/><br><input type=\"submit\" value=\"Cambiar\" style='width:100px; height:50px'/></form></body></html>";
+String web = "<!DOCTYPE HTML><html><head><title>Lampara</title></head><body><h1> <FONT SIZE=\"5\" COLOR=\"red\"> Colores </h1>Selecciona un color: <form action=\"\"><input name=\"color\" type=\"color\" style='width:300px; height:300px'/><input name=\"intensidad\" type=\"range\" style='width:80%; height:300px'/><br><input type=\"submit\" value=\"Cambiar\" style='width:100px; height:50px'/></form></body></html>";
 
 //Convertir Hexadecimal a Decimal
 //Funcion extraida de https://github.com/benrugg/Arduino-Hex-Decimal-Conversion/blob/master/hex_dec.ino
 unsigned int hexToDec(String hexString) {
-
+  
   unsigned int decValue = 0;
   int nextInt;
-
+  
   for (int i = 0; i < hexString.length(); i++) {
-
+    
     nextInt = int(hexString.charAt(i));
     if (nextInt >= 48 && nextInt <= 57) nextInt = map(nextInt, 48, 57, 0, 9);
     if (nextInt >= 65 && nextInt <= 70) nextInt = map(nextInt, 65, 70, 10, 15);
     if (nextInt >= 97 && nextInt <= 102) nextInt = map(nextInt, 97, 102, 10, 15);
     nextInt = constrain(nextInt, 0, 15);
-
+    
     decValue = (decValue * 16) + nextInt;
   }
-
+  
   return decValue;
 }
 
-void iluminacion(int R, int G, int B){
-   for(int i=0;i<NUMPIXELS;i++){
+void iluminacion(int R, int G, int B, int I){
+   for(int i=0;i<NUMPIXELS;i++){   
     pixels.setPixelColor(i, pixels.Color(R,G,B));
-    pixels.show();
+    pixels.setBrightness(I);
+    pixels.show(); 
     delay(200);
   }
 }
@@ -70,30 +77,45 @@ void handleRoot() {
     colores += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
   Serial.println(colores);
-
+  
   //Busca el comienzo del codigo hexadecimal de color
   int firstHastag = colores.indexOf('#');
   Serial.print("Primera respuesta: ");Serial.println(firstHastag);
-
+  
   //Extrae los colores codificados en Hexadecimal en R-G-B
   R = colores.substring(firstHastag+1, firstHastag+3);
   G = colores.substring(firstHastag+3, firstHastag+5);
   B = colores.substring(firstHastag+5, firstHastag+7);
-  Serial.print(R);Serial.print("-"); Serial.print(G);Serial.print("-");Serial.println(B);
-
+  Serial.print("Color en Hex: ");Serial.print(R);Serial.print("-"); Serial.print(G);Serial.print("-");Serial.println(B);
+  
   //Convierte los valores de color hexadecimales en decimales
   R = hexToDec(R);
   G = hexToDec(G);
-  B = hexToDec(B);
-  Serial.print(R);Serial.print("-"); Serial.print(G);Serial.print("-");Serial.println(B);
-
+  B = hexToDec(B);  
+  Serial.print("Color en RGB: ");Serial.print(R);Serial.print("-"); Serial.print(G);Serial.print("-");Serial.println(B);
+  
   //Transforma el String en valores integer
   Ri = R.toInt();
   Gi = G.toInt();
   Bi = B.toInt();
 
+  //Obtenemos la intensidad
+  int findIntensidad = colores.indexOf('intensidad');
+  int ultPos = colores.length();
+  //int ultPos = colores.lastIndexOf('/');
+  Serial.print("Comienza la cadena");Serial.println(findIntensidad);Serial.print("Posicion");Serial.println(ultPos);
+  intensidad = colores.substring(findIntensidad+4,ultPos-1);
+  Ii = intensidad.toInt();
+  Serial.print("Intensidad: ");Serial.println(intensidad);
+  Serial.print("I: ");Serial.println(Ii);
+  
+  //mapeamos los valores del range HTML5 (0 a 100) a brightness de Neopixel (0 a 255)
+  Ii = map(Ii, 0, 100, 0, 255);
+  Serial.print("Intensidad mapeada: ");Serial.println(Ii);
+  
   //Encendemos la luces en el color seleccionado
-  iluminacion(Ri,Gi,Bi);
+  iluminacion(Ri,Gi,Bi,Ii);
+ 
 }
 
 void handleNotFound(){
@@ -117,7 +139,7 @@ void setup(void){
   #if defined (__AVR_ATtiny85__)
     if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
   #endif
-
+  
   pinMode(led, OUTPUT);
   digitalWrite(led, 0);
   Serial.begin(115200);
@@ -159,17 +181,17 @@ void loop(void){
   if (!client) {
     return;
   }
-
+  
   // Wait until the client sends some data
   Serial.println("new client");
   while(!client.available()){
     delay(1);
   }
-
+  
   // Read the first line of the request
   String req = client.readStringUntil('\r');
   Serial.println(req);
 
   client.flush();
-
+  
 }
